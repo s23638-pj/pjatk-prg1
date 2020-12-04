885a149f759f0789c60d858cf12281db2ffcf236
Wiktor Krieger <s23638@pjwstk.edu.pl>
2020-12-04 23:41:24 +0100

    Naprawa s03-guessing-game

--- src/s03-guessing-game.cpp
+++ src/s03-guessing-game.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
 #include <random>
-<<<<<<< HEAD
 
 auto ask_user_for_integer(std::string prompt) -> int
 {
 	std::cout << prompt << "guess:";
-=======
-/*
-auto ask_user_for_integer(std::string) -> int
-{
->>>>>>> a458242eaddd01f92ac980d390d1171d108d0940
 	auto a = std::string{};
 	std::getline(std::cin, a);
 	return std::stoi(a);
 }
-<<<<<<< HEAD
 
-=======
-*/
->>>>>>> a458242eaddd01f92ac980d390d1171d108d0940
 auto main() -> int
 {
 	auto a = 0;
@@ -27,13 +17,7 @@ auto main() -> int
 	auto const b = d100(rd);
 	do
 	{
-<<<<<<< HEAD
 		auto const a = ask_user_for_integer("");
-=======
-		std::cout << "guess: ";
-		//auto a = ask_user_for_integer;
-		std::cin >> a;
->>>>>>> a458242eaddd01f92ac980d390d1171d108d0940
 
 		if( a < b )
 		{
